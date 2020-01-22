28997478a5c2f5e20834d2e8898b42366fe12161
user <user@ubuntu.local>
2020-01-22 14:58:26 -0600

    Unpolished,runs

--- IntegerSequence/main.cpp
+++ IntegerSequence/main.cpp
@@ -5,16 +5,19 @@ using namespace std;
 int main()
 {
     int n;
-    int answerCount;
     int first;
     int last;
-    int runningSum;
+    int runningSum = 0;
 
     cout << "This program calculates the sum of integers between a starting and ending number";
     cout << "Please enter the beginning number:";
     cin >> first;
     cout << "Please enter the ending number:";
     cin  >> last;
-    //adding a line of code
 
+        for (n = first; n <= last; n++) {
+        runningSum = runningSum + n;
+    }
+
+    cout << runningSum;
 }
