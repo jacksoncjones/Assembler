3bbecdcc20c0900d9f508e91d15e814126585638
jacksoncjones <60197974+jacksoncjones@users.noreply.github.com>
2020-04-06 14:56:36 -0500

    OhmsLawPush

--- ebe/CalculateVoltage.asm
+++ ebe/CalculateVoltage.asm
@@ -1,18 +1,47 @@
             segment .data
-voltage     dq  0               ;the value to calculate
-amperage    dq  10              ;the amperage input
-resistance  dq  5               ;the resistance input
+voltage     dq  0                   ;the value to calculate
+amperage    dq  10                  ;the amperage input
+resistance  dq  6                   ;the resistance input
 
             segment .text
-            global main         ;Tell linker about main
+            global main             ;Tell linker about main
 main:
-                                ;move variables to registers
-            mov rax, amperage
-            mov rbx, resistance
+                                    ;move variables to registers
+            mov rax, [voltage]
+            mov rbx, [resistance]
+            mov rcx, [amperage]
     
-            imul rax, rbx       ;do computation
-                        
-            mov [voltage], rax ;store answer in the voltage variable
+                             
+                                    ;if voltage is zero, calculate it
+            voltage_section:
+            cmp rax, 0              ;compare to zero
+            jne resistance_section  ;jump to resistance if not calculating
+            imul rbx, rcx           ;multiply resistance and amperage
+            mov [voltage], rbx      ;define resistance to rax
             
-            mov rbx, 0          ;untrash everything
+            jmp end
+            
+            
+                                    ;if resistance is zero, calculate it
+            resistance_section:
+            cmp rbx, 0
+            jne amperage_section
+            mov rdx, 0              ;clear rdx
+            idiv rcx                ;divide rax by rcx, stores in rax, remainder in rdx
+            mov [resistance], rax   ;define resistance to rax
+            
+            jmp end                                                                                                                       
+              
+                                                                                                                                                                                                          
+                                    ;if amperage is zero, calculate it
+            amperage_section:
+            mov rdx, 0              ;clear rdx
+            idiv rbx                ;divides rax by rbx, stores in rax, remainders in rdx
+            mov [amperage], rax     ;define amperage to rax
+            
+            jmp end
+            
+                                     
+            
+            end:
             mov rax, 0
